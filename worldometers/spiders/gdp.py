08ebe8792e6b5d4c718bda46a6868dba91f04e5e
Carlos Cavalcanti <crl.albu@gmail.com>
2020-10-27 00:23:43 -0300

    gdp parse

--- worldometers/spiders/gdp.py
+++ worldometers/spiders/gdp.py
@@ -7,4 +7,13 @@ class GdpSpider(scrapy.Spider):
     start_urls = ['http://worldpopulationreview.com/countries/countries-by-national-debt/']
 
     def parse(self, response):
-        pass
+        countries = response.xpath("//td/a")
+        for country in countries:
+            name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
+
+            # absolute_url = f"https://worldpopulationreview.com{link}"
+            #absolute_url = response.urljoin(link)
+
+            # yield scrapy.Request(url=absolute_url)
+            yield response.follow(url=link)
